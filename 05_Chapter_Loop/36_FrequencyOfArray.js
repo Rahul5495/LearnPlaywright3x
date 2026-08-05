cc8cbcab9f1da07e151011ddfd04c3c202d159e9
Rahul5495 <rahul5495@github.com>
2026-08-05 11:52:27 +0530

    Replace FrequencyOfArray with While_Loop exercise
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

--- 05_Chapter_Loop/36_FrequencyOfArray.js
+++
@@ -1,9 +0,0 @@
-
-let arr=[1,2,1,3,2]
-const frequency = {};
-for (let element of arr) {
-    frequency[element] = (frequency[element] || 0) + 1;
-}
-for (let key in frequency) {
-    console.log(`${key} appears ${frequency[key]} times`);
-}
